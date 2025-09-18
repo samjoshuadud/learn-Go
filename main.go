90a719a1f675066e70c34dae68fe8355ccf7cb2b
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2025-09-18 23:57:09 +0800

    first

---
+++ main.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/users", GetUsers).Methods("GET")	
+	r.HandleFunc("/users/{id}", GetUser).Methods("GET")
+	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+	r.HandleFunc("/users", CreateUser).Methods("POST")
+	
+
+	fmt.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))	
+}
